64f64112054585ae102b7dfd386207f02f02c1f7
Saadaoui.skander <skandersaadaoui@esprit.tn>
2022-10-09 17:00:50 +0200

    Modification stock

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Stock implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+		// Test Modif
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idStock;
